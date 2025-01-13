a9a15529bd4eb5a56e7725ec503823d512f93032
Guste Gaubaite <219.guste@gmail.com>
2025-01-13 17:06:09 +0100

    Refactor square component to accept props and integrate board component with click handling for updating square values

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,15 @@
 import { FC } from "react";
 import { useState } from "react";
 
-const Square: FC = () => {
-  const [value, setValue] = useState(null);
+interface SquareProps {
+  value: string | null;
+  onSquareClick: () => void;
+}
 
-  const handleClick = () => {
-    setValue("X");
-  };
+const Square: FC<SquareProps> = ({ value, onSquareClick }) => {
   return (
     <>
-      <button className="square" onClick={handleClick}>
+      <button className="square" onClick={onSquareClick}>
         {value}
       </button>
     </>
@@ -17,22 +17,30 @@ const Square: FC = () => {
 };
 
 export const Board: FC = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = (index) => {
+    const nextSquares = squares.slice();
+    nextSquares[index] = "X";
+    setSquares(nextSquares);
+  };
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
