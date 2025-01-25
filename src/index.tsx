10536c07b40770b5506cc60934e5b85920f82699
Guste Gaubaite <219.guste@gmail.com>
2025-01-25 16:17:30 +0100

    Move files to another folder

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { MainComponent } from "./app";
-import "./mystyles.scss";
-
-const root = createRoot(document.getElementById("root"));
-
-root.render(<MainComponent />);
