09a2bd5d3dee0764b23fab3508dfc094acf6477c
Guste Gaubaite <219.guste@gmail.com>
2025-01-25 16:59:31 +0100

    Add login form with validation and navigation to Listpage on succesful login

--- practice 2/react-app/src/components/login.tsx
+++ practice 2/react-app/src/components/login.tsx
@@ -1,16 +1,43 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   const navigate = useNavigate();
 
-  const handleNavigation = () => {
-    navigate("/list");
+  const handleNavigation = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (username === "admin" && password === "test") {
+      navigate("/list");
+    } else {
+      alert("User / password not valid...");
+    }
   };
 
   return (
     <>
-      <h1>Login Page</h1>
-      <button onClick={handleNavigation}>Login</button>
+      <h1>Login</h1>
+      <form onSubmit={handleNavigation}>
+        <div>
+          <label>Username</label>
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Enter</button>
+      </form>
     </>
   );
 };
