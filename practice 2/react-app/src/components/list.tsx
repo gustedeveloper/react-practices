507cbf4720629258c15a80b09adef23d99b2f313
Guste Gaubaite <219.guste@gmail.com>
2025-01-25 17:27:56 +0100

    Fetch and display Lemoncode members from GitHub API in ListPage

--- practice 2/react-app/src/components/list.tsx
+++ practice 2/react-app/src/components/list.tsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+interface Member {
+  login: string;
+  id: string;
+  avatar_url: string;
+}
+
 export const ListPage = () => {
+  const [members, setMembers] = useState<Member[]>([]);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/orgs/lemoncode/members`)
+      .then((response) => response.json())
+      .then((json) => setMembers(json));
+  });
+
   return (
     <>
       <h1>List Page</h1>
       <Link to="/detail">Navigate to Detail Page</Link>
+      <div>
+        {members.map((member) => (
+          <div key={member.id}>
+            <span>{member.login}</span>
+            <img src={member.avatar_url} />
+          </div>
+        ))}
+      </div>
     </>
   );
 };
