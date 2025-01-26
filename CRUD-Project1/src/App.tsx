435080f477e230285a7e79a6cf2d0f2cc9688b37
Guste Gaubaite <219.guste@gmail.com>
2025-01-26 20:08:31 +0100

    Implement add new task functionality

--- CRUD-Project1/src/App.tsx
+++ CRUD-Project1/src/App.tsx
@@ -6,6 +6,20 @@ import { Task } from "./model";
 export const App = () => {
   const [tasks, setTasks] = useState<Task[]>(tasksData.tasks);
   const [inputValue, setInputValue] = useState("");
+
+  const addNewTask = () => {
+    if (inputValue.trim() !== "") {
+      const newTask: Task = {
+        id: tasks.length + 1,
+        title: inputValue,
+        completed: false,
+      };
+      setTasks([...tasks, newTask]);
+    } else {
+      alert("Enter new task!");
+    }
+  };
+
   return (
     <div>
       <h1>Tasks manager</h1>
@@ -15,7 +29,7 @@ export const App = () => {
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
-        <button>Add</button>
+        <button onClick={addNewTask}>Add</button>
       </div>
       <ul>
         {tasks.map((task) => (
