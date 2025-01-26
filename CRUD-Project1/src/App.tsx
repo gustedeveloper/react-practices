2882fb8245ff8659277a007ee20005a5e275af83
Guste Gaubaite <219.guste@gmail.com>
2025-01-26 19:58:53 +0100

    Implement basic task manager list component from tasks data json

--- CRUD-Project1/src/App.tsx
+++ CRUD-Project1/src/App.tsx
@@ -1,11 +1,31 @@
-import "./App.css";
+import "./App.scss";
+import { useState } from "react";
+import tasksData from "./data/tasks.json";
+import { Task } from "./model";
 
-function App() {
+export const App = () => {
+  const [tasks, setTasks] = useState<Task[]>(tasksData.tasks);
+  const [inputValue, setInputValue] = useState("");
   return (
-    <>
-      <div></div>
-    </>
+    <div>
+      <h1>Tasks manager</h1>
+      <div className="add-container">
+        <label>Add new task</label>
+        <input
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button>Add</button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <span>{task.id}.</span>
+            <span className="title">{task.title}</span>
+            <span> Completed: {task.completed ? "yes" : "no"}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-}
-
-export default App;
+};
